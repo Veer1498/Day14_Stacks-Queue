e0a3c11ace8855c169bb9b0f4a96d7e7e8a390e8
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-12 03:40:00 +0530

    Added : Ability to Create a Stack Using LinkedList

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Stacks & Queue Program");
+		Stacks stack = new Stacks();
+		
+		stack.addNode();
 
 	}
 
--- src/stack/Stacks.java
+++ src/stack/Stacks.java
@@ -1,5 +1,22 @@
 package stack;
 
+import linkedList.LinkedList;
+import linkedList.Node;
+
 public class Stacks {
+	private Node top;
+	private LinkedList linkedList;
+	
+	public Stacks() {
+		this.linkedList = new LinkedList();
+	}
+	
+	public void addNode () {
+		linkedList.addNode(70);
+		linkedList.addNode(30);
+		top = linkedList.addNode(56);
+		linkedList.printLinkedList();
+		
+	}
 
 }
