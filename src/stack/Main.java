4cf7b58df95f1e17741ae555bb64cc402eccbb11
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-12 17:03:58 +0530

    Added : Ability to Peak and Pop all The elements in Stack

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -1,13 +1,38 @@
 package stack;
+import java.util.Scanner;
+/**
+ * @author singa
+ *
+ */
 
 public class Main {
+	/**
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
+		/*
+		 * Created an Object Stack
+		 * Took a Scanner to Select Which Method to Perform
+		 * Calling Methods from Stacks Class and perform.
+		 */
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Stacks & Queue Program");
 		Stacks stack = new Stacks();
 		
-		stack.addNode();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter \n1.Add Elements in Stack \n2.Peak and Pop Elements in Stack");
+		int n = sc.nextInt();
+		
+		switch(n) {
+		case 1 :
+			stack.push();
+			break;
+		case 2 :
+			stack.Peak();
+			stack.pop();
+			break;
+		}
 
 	}
 
